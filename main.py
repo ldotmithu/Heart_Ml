fae946a84338d0bd5bd033568561466518d12d7f
ldotmithu <ldotmithurshan222@gmail.com>
2024-09-02 00:48:52 +0530

    train

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validataion import DataValidationTrainingPipeline
 from src.mlProject.pipeline.stage_03_data_trnsfomation import DataTransformationTrainingPipeline
+from src.mlProject.pipeline.stage_04_model_train import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -30,6 +31,16 @@ STAGE_NAME = "Data trnsfomation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+STAGE_NAME = "Model Train stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
