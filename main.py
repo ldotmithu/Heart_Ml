cba220fcbf6ab381fb22bc00f8ca65ee43f2448a
ldotmithu <ldotmithurshan222@gmail.com>
2024-09-01 20:49:30 +0530

    data_ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlProject import logger
+from mlProject.pipeline.satage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome logging function')
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
